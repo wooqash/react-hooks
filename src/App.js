7fac8dda6cd8927f5bd0970166aef63499c478ff
lsobola <lukasz.sobola@grandparade.co.uk>
2022-11-09 17:38:45 +0100

    added couple useEffect hooks with some descriptions

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+
+import {useState,  useEffect } from 'react';
 import './App.css';
 
  // below function can be used to speed up compute initial state
@@ -14,8 +15,11 @@ function App() {
   //   console.log('run function');
   //   return 4
   // });
-  
+
   const [count, setCount] = useState(4);
+  const [resourceType, setResourceType] = useState('posts');
+  const [items, setItems] = useState([]);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
   function decrementCount() {
     setCount((prevCount) => prevCount - 1);
@@ -24,11 +28,43 @@ function App() {
     setCount((prevCount) => prevCount + 1);
   }
 
+  function handleResize() {
+    setWindowWidth(window.innerWidth);
+  }
+
+  useEffect(() => {
+    console.log('render');
+  }, [resourceType]);
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+      .then(response => response.json())
+      .then(json => setItems(json))
+  }, [resourceType]);
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => { //clean up function is called out before every useEffect invoked
+      window.removeEventListener('resize', handleResize); //to avoid memory leaks
+    }
+  }, [])
+
   return (
     <>
+      <h1>React HOOKS</h1>
+      <h2>useState</h2>
       <button onClick={decrementCount}>-</button>
       <span>{count}</span>
       <button onClick={incrementCount}>+</button>
+      <br />
+      <br />
+      <hr />
+      <h2>useEffect</h2>
+      <button onClick={() => setResourceType('posts')}>Posts</button>
+      <button onClick={() => setResourceType('users')}>Users</button>
+      <button onClick={() => setResourceType('comments')}>Comments</button>
+      <p>Window width: {windowWidth}</p>
+      {items.map(item => (<pre key={item.id}>{JSON.stringify(item)}</pre>))}
     </>
   );
 }
